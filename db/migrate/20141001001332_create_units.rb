79a4098a7614392827494e5b854c74b2bee5cd2a
Christopher Mehmed <chrismehmed@gmail.com>
2014-09-30 20:27:59 -0400

    Units model and controller

---
+++ db/migrate/20141001001332_create_units.rb
@@ -0,0 +1,16 @@
+class CreateUnits < ActiveRecord::Migration
+  def change
+    create_table :units do |t|
+    	t.string :type
+    	t.string :order_id
+    	t.string :vendor
+    	t.string :mfg
+    	t.string :product_id
+    	t.string :nickname
+    	t.string :description
+    	t.string :notes
+    	t.integer :product_type
+      t.timestamps
+    end
+  end
+end
