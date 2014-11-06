763dbead279196833bc8b0b26204c39fd2516961
Christopher Mehmed <chrismehmed@gmail.com>
2014-11-06 13:32:41 -0500

    validation rj travel

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,14 @@
 class Address < ActiveRecord::Base
 
+	validates :state,
+		:length => { is: 2, :message => "Use two letter state code." }, allow_blank: true
+
+	validates :zip,
+		:allow_blank => true,
+		:numericality => 
+			{:message => "Use numbers for the zip code."},
+		:length => { is: 5, :message => "Zip code should have 5 digits." }
+
 	belongs_to :locatable, polymorphic: true
 
 end
--- app/models/travel.rb
+++ app/models/travel.rb
@@ -1,5 +1,36 @@
 class Travel < ActiveRecord::Base
 
+	validates :day,
+		:presence => 
+		{:message => "Day of Departure can't be left blank, and must follow the recommended format."
+
+		}
+
+	validates :depart_city,
+		:presence => 
+		{:message => "Depart From can't be left blank"
+
+		}
+
+	validates :depart_time,
+		:presence => 
+		{:message => "Time of Departure can't be left blank, and must follow the recommended format."
+
+		}
+
+	validates :arrive_city,
+		:presence => 
+		{:message => "Traveling To can't be left blank"
+
+		}
+
+	validates :arrive_time,
+		:presence => 
+		{:message => "Time of Arrival can't be left blank, and must follow the recommended format."
+
+		}
+
+
 	belongs_to :crew
 
 end
